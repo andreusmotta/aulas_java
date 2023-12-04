ba6c6da5ed66b694e91a8c8f4d12aa4827e8fc81
unknown <andreus.motta@gmail.com>
2023-12-04 13:22:17 -0300

    Adicionando aulas.

---
+++ aulas_basicas/aulas/aula04/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Oi oi oi");
+        // System.out.print("Oi novamente");        
+        // System.out.println("Oi mundo");
+        // System.out.println("Olá");
+        // System.out.println("Hello, World!");
+    }
+}
